e4766db507f058c4d2e8803d5ab99a1f686a1b16
aarafiit <103585742+aarafiit@users.noreply.github.com>
2024-03-07 14:52:24 +0600

    Time: 3 ms (42.17%), Space: 7.5 MB (67.85%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    bool isValid(string s) {
+        
+       stack<char> st; 
+        for (char c : s) { 
+            if (c == '(' || c == '{' || c == '[') { 
+                st.push(c); 
+            } else { 
+                if (st.empty() || 
+                    (c == ')' && st.top() != '(') || 
+                    (c == '}' && st.top() != '{') ||
+                    (c == ']' && st.top() != '[')) {
+                    return false;
+                }
+                st.pop(); 
+            }
+        }
+        return st.empty(); 
+    }
+};
